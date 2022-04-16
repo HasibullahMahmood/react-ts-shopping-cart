89c5ff824fce7a628e1cd839640c32f3d720ce1b
Hasibullah Mahmood 2 <hasibullahmahmod@gmail.com>
2022-04-16 10:18:46 +0300

    components imported and fetch data is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-function App() {
+// components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCart from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge';
+
+// custom components
+import { Wrapper } from './App.styles';
+
+// types
+import CartItemType from 'models/CartItemType';
+
+const getProducts = async (): Promise<CartItemType[]> =>
+	await (await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+	const { data, isLoading, error } = useQuery('products', getProducts);
+
+	if (isLoading) return <LinearProgress />;
+	if (error) return <div>Something went wrong...</div>;
+
 	return <div>Hello world</div>;
-}
+};
 
 export default App;
---
+++ src/models/CartItemType.ts
@@ -0,0 +1,11 @@
+type CartItemType = {
+	id: number;
+	category: string;
+	description: string;
+	image: string;
+	price: number;
+	title: string;
+	amount: number;
+};
+
+export default CartItemType;
